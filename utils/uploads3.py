3acb72c0b7c50a80617842e1c0af8edcc9d9215f
Andres <andresinsuastyd10@gmail.com>
2020-12-16 18:05:55 -0500

    codigo ejemplo para subir archivos a s3 y mejora de requirements.txt

--- utils/uploads3.py
+++ utils/uploads3.py
@@ -2,7 +2,15 @@ import boto3
 
 s3 = boto3.resource('s3')
 
+# list files
 print('='*50)
 print('List all the buckets created on your account')
 for id, bucket in enumerate(s3.buckets.all()):
     print(id+1,bucket.name)
+
+# Upload a new file
+bucket_name = 'pruebafastapi'
+filename = 'requirements.txt'
+data = open(filename,'rb')
+s3.Bucket(bucket_name).put_object(Key=filename,Body=data)
+
